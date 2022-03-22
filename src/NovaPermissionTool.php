c61dca51395348efc48fb48bfc5531346449a07e
Fernando Canteiro <canteiro.fernando@gmail.com>
2022-03-22 14:28:23 +0000

    init

---
+++ src/NovaPermissionTool.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Workup\NovaPermission;
+
+use Gate;
+use Laravel\Nova\Nova;
+use Laravel\Nova\Tool;
+
+class NovaPermissionTool extends Tool
+{
+    public string $roleResource = Role::class;
+    public string $permissionResource = Permission::class;
+
+    public string $rolePolicy = RolePolicy::class;
+    public string $permissionPolicy = PermissionPolicy::class;
+
+    /**
+     * Perform any tasks that need to happen when the tool is booted.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::resources([
+            $this->roleResource,
+            $this->permissionResource,
+        ]);
+
+        Gate::policy(config('permission.models.permission'), $this->permissionPolicy);
+        Gate::policy(config('permission.models.role'), $this->rolePolicy);
+    }
+
+    public function roleResource(string $roleResource): NovaPermissionTool
+    {
+        $this->roleResource = $roleResource;
+
+        return $this;
+    }
+
+    public function permissionResource(string $permissionResource): NovaPermissionTool
+    {
+        $this->permissionResource = $permissionResource;
+
+        return $this;
+    }
+
+    public function rolePolicy(string $rolePolicy): NovaPermissionTool
+    {
+        $this->rolePolicy = $rolePolicy;
+
+        return $this;
+    }
+
+    public function permissionPolicy(string $permissionPolicy)
+    {
+        $this->permissionPolicy = $permissionPolicy;
+
+        return $this;
+    }
+}
